ef6fb0994c539b6c23fd00920c1557156a4d71a7
Khouildi Iyed <iyedkhouildi12@gmail.com>
2024-02-24 15:50:56 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -29,6 +29,8 @@ app.use('/message', messagingRoutes);
 app.use('/comment-course', commentingRoutes);
 app.use('/courses', courseRouter);
 app.use('/api/faq', faqRouter);
+
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
