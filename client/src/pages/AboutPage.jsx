e3eee2c825dbe7a070e2bf79285104ffff84dd06
Khouildi Iyed <iyedkhouildi12@gmail.com>
2024-04-28 11:15:38 +0100

    about page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -7,14 +7,15 @@ import features from "../components/assests/images/courses8.jpg";
 import NavBar from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import { CgMail } from "react-icons/cg";
-
-
+import career from '../components/assests/images/Career_Center.png';
+import career2 from '../components/assests/images/Career_Center_portfolio.png';
+import career3 from '../components/assests/images/Career_Center_Professional_certifications__.png';
 const Enroll = () => {
     return (
       <div>
         
         <NavBar/>
-    <section>
+        <section>
       <Container>
         <Row>
           <Col lg="6" md="6">
@@ -36,9 +37,9 @@ const Enroll = () => {
           </Col>
         </Row>
       </Container>
-    </section>
+        </section>
 
-    <section class="about">
+        <section class="about">
     <div class="section" id="about">
     <div class="grid-container">
       <div class="border-container">
@@ -56,8 +57,9 @@ const Enroll = () => {
       </div>
     </div>
     </div>
-    </section>
-    <section className='features'>
+        </section>
+
+        <section className='features'>
       <Container>
         <Row>
           <Col lg="6" md="6">
@@ -110,8 +112,9 @@ const Enroll = () => {
           </Col>
         </Row>
       </Container>
-    </section>
-    <div class="contact" id="contact">
+        </section>
+
+        <div class="contact" id="contact">
         <div class="text-center max-w-600 mx-auto">
             <div class="title" >
                  Subscribe Newsletter
@@ -124,9 +127,40 @@ const Enroll = () => {
       <button class="btns">Subscribe</button>
     </form>
   </div>
-    </div>
-
-   
+        </div>
+        <div className="job">
+          <div className="tit">
+            <h2>Get job-ready</h2>
+            <p>Showcase your skills with portfolio projects and a professional certification, then see if you’re ready to apply to your dream job.</p>
+          </div>
+          <div className="grid">
+            <div className="grid1">
+              <div className="feature-card">
+               <img src={career}  className="feature-card__image" />
+               <h2 className="">Job-readiness checker</h2>
+               <p className="">Use AI to evaluate how well you meet the requirements for a given role based on your skills and experience.</p>
+                <button className="btn-">Try it out</button>
+              </div>
+            </div>
+            <div className="grid2">
+            <div className="feature-card">
+               <img src={career2}  className="feature-card__image" />
+               <h2 className="">Portfolio projects</h2>
+               <p className="">Apply what you’re learning to create robust, recruiter-ready projects for your portfolio.</p>
+                <button className="btn-">Explore projects</button>
+              </div>
+            </div>
+            <div className="grid3">
+            <div className="feature-card">
+               <img src={career3}  className="feature-card__image" />
+               <h2 className="">Certifications</h2>
+               <p className="">Prove your expertise to yourself and prospective employers — pass all exams in select career paths to earn a professional certification.</p>
+                <button className="btn-">Learn more</button>
+              </div>
+            </div>
+          </div>
+          
+        </div>  
         <Footer/>
        
       </div>
